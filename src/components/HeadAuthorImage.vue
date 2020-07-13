0b987da64b756eca5a3038cc1bb078eff05f4ef0
ryanflowers-developer <51009900+ryanflowers-developer@users.noreply.github.com>
2020-07-13 15:17:04 -0400

    Fixed author head image

--- src/components/HeadAuthorImage.vue
+++ src/components/HeadAuthorImage.vue
@@ -1,7 +1,7 @@
 <template>
 
 
-  <v-card class="mx-auto" width="40%"><v-img src="https://caytlawson.com/images/index-author.jpeg"></v-img></v-card>
+  <v-card class="mx-auto" width="40%"><v-img src="https://i.imgur.com/VYUnW4A.jpg"></v-img></v-card>
 
 
 </template>
